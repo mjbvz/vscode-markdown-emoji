e2fae13a8d8687467a975e0c1a5bdd5dd8c37a72
Matt Bierner <matb@microsoft.com>
2017-08-11 01:04:34 -0700

    Fix syntax error

--- extension.js
+++ extension.js
@@ -1,10 +1,10 @@
 'use strict'
-import * as vscode from 'vscode'
 
-export function activate(context) {
+function activate(context) {
     return {
         extendMarkdownIt(md) {
             return md.use(require('markdown-it-emoji'));
         }
-    }
+    };
 }
+exports.activate = activate;
